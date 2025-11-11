4cfb331a21a3cf1ec18be077cf12d966509ea38f
Kaung Khant Win <kkw.kaungkhant@gmail.com>
2025-11-11 11:27:20 +0700

    feat(app): add species detail page

---
+++ src/app/species/[binomialName]/page.tsx
@@ -0,0 +1,12 @@
+import SpeciesDetail from '@/components/app/species/SpeciesDetail/SpeciesDetail'
+import React from 'react'
+
+const page = async () => {
+    return (
+        <div>
+            <SpeciesDetail />
+        </div>
+    )
+}
+
+export default page
