a0064adb565be4173dfbbe469f40a31d16fbe1ce
Kaung Khant Win <kkw.kaungkhant@gmail.com>
2025-11-14 11:37:10 +0700

    feat(components): add TakeAction components to Endangered page

--- src/app/endangered/page.tsx
+++ src/app/endangered/page.tsx
@@ -1,6 +1,7 @@
 import EndangeredAnimals from "@/components/app/endangered/EndangeredAnimals/EndangeredAnimals"
 import EndangeredIntro from "@/components/app/endangered/EndangeredIntro/EndangeredIntro"
 import HelpThem from "@/components/app/endangered/HelpThem/HelpThem"
+import TakeAction from "@/components/app/endangered/TakeAction /TakeAction"
 import Sources from "@/components/common/Sources"
 
 const page = () => {
@@ -8,6 +9,7 @@ const page = () => {
         <div>
             <EndangeredIntro />
             <EndangeredAnimals />
+            <TakeAction />
             <HelpThem />
             <Sources />
         </div>
---
+++ src/data/takeActionData.ts
@@ -0,0 +1,27 @@
+import adopt from "../../public/endangered/TA_ADOPT.jpg";
+import tipping from "../../public/endangered/TA_TIPPING-POINT.jpg";
+import protect from "../../public/endangered/TA_PROTECT.jpg";
+
+export const takeActionData = [
+    {
+        name: "A GIFT THAT CONNECTS GENERATIONS",
+        description: "This Christmas, help protect nature and give a gift that lasts. Whether it’s for a child discovering the wonders of nature or someone who loves animals, a WWF animal adoption lasts far beyond the festive season.",
+        linkText: "ADOPT AN ANIMAL",
+        link: "https://support.wwf.org.uk/adoptions",
+        photo: adopt
+    },
+    {
+        name: "GLOBLE TIPPING POINTS REPORT",
+        description: "This is a 999 for nature. Climate change is pushing our world towards dangerous tipping points, and we must act now.",
+        linkText: "FIND OUT MORE",
+        link: "https://www.wwf.org.uk/articles/global-tipping-points-report",
+        photo: tipping
+    },
+    {
+        name: "PROTECT WHAT MATTERS",
+        description: "Around the world, vast areas of forest are being destroyed every day. Help us to speak up for forests and all those who depend on them.",
+        linkText: "EMAIL YOUR MP",
+        link: "https://www.wwf.org.uk/act/living-planet-act/ask-mp-forests",
+        photo: protect
+    }
+]
