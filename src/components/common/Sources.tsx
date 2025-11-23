15ca33fa2b9a0e5e54736c053a7ad37960c7b053
Kaung Khant Win <kkw.kaungkhant@gmail.com>
2025-11-23 14:40:15 +0700

    refactor(styles): refactor Sources component and add a styling

--- src/components/common/Sources.tsx
+++ src/components/common/Sources.tsx
@@ -9,40 +9,43 @@ const Sources = () => {
     const [open, setOpen] = useState<boolean>(false);
 
     return (
-        <div className="w-full h-auto px-5 pb-25 pt-10">
-            <button onClick={() => setOpen((prev) => !prev)} 
-                    className="w-full"
-                >
-                { 
-                    open ? 
-                        <div className="flex justify-between text-md border-t-2 border-black dark:border-white">
-                            <p>Sources</p>
-                            <p>▼</p>
-                        </div>
-                    : 
-                        <div className="flex justify-between text-md border-t-2 border-black dark:border-white">
-                            <p>Sources</p>
-                            <p>▲</p>
-                        </div>
-                }
-            </button>
-            <div
-                className={`absolute mt-2 transition-opacity duration-300 ${
-                open ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
-                }`}
+        <div className="w-full h-auto px-5 py-10">
+            <button 
+                onClick={() => setOpen((prev) => !prev)} 
+                className="w-full"
             >
-                {
-                    resourcesData.map((data) => (
-                        <div className="flex flex-col flex-start gap-3 " key={data.name}>
-                           <Link 
+                {open ? (
+                    <div className="font-mono bg-black px-3 text-white dark:bg-white dark:text-black text-xl font-semibold py-2 flex justify-between text-md border-t-2 border-black dark:border-white">
+                        <p>Sources</p>
+                        <p>▼</p>
+                    </div>
+                ) : (
+                    <div className="font-mono text-xl px-3 font-semibold py-2 flex justify-between text-md border-t-2 border-black dark:border-white">
+                        <p>Sources</p>
+                        <p>▲</p>
+                    </div>
+                )}
+            </button>
+
+            <div
+                className={`transition-all duration-500 overflow-hidden 
+                    ${open ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"}
+                `}
+            >
+                <div className="mt-2">
+                    {resourcesData.map((data) => (
+                        <div className="flex flex-col flex-start gap-3 px-5" key={data.name}>
+                            <Link 
                                 href={data.link} 
                                 target="_blank"
-                                rel="noopener noreferrer">
-                                {data.link} 
+                                rel="noopener noreferrer"
+                                className="font-mono text-xl tracking-wide hover:underline hover:text-[#005A25]"
+                            >
+                                {data.link}
                             </Link>
                         </div>
-                    ))
-                }
+                    ))}
+                </div>
             </div>
         </div>
     )
